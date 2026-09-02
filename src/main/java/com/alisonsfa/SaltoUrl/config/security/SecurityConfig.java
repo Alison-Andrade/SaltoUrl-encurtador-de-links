21fa0591b56bb875cee5a778077fb56e699a2704
alison-andrade <sidneialison@gmail.com>
2026-09-02 20:39:34 -0300

    feat: adiciona configuração do RabbitMQ com usuário e senha padrão, atualiza segurança com AuthenticationManager

--- src/main/java/com/alisonsfa/SaltoUrl/config/security/SecurityConfig.java
+++ src/main/java/com/alisonsfa/SaltoUrl/config/security/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.alisonsfa.SaltoUrl.config.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -24,22 +26,22 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf(AbstractHttpConfigurer::disable)
+        return http
+            .csrf(csrf -> csrf.disable())
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers(HttpMethod.POST, "/auth/register", "/auth/login").permitAll()
-                
                 .requestMatchers(HttpMethod.GET, "/{code:[a-zA-Z0-9]+}").permitAll()
-                
                 .anyRequest().authenticated()
             )
-            
-            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
-
-        return http.build();
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+            .build();
     }
 
+    @Bean 
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
     
     @Bean
     public PasswordEncoder passwordEncoder() {
