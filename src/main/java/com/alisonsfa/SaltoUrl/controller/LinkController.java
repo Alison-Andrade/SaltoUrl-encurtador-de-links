3ca4b68a69e482f006f78433260b32db834fffe4
alison-andrade <sidneialison@gmail.com>
2026-09-06 11:00:50 -0300

    refactor: remove imports não utilizados no LinkController

--- src/main/java/com/alisonsfa/SaltoUrl/controller/LinkController.java
+++ src/main/java/com/alisonsfa/SaltoUrl/controller/LinkController.java
@@ -1,13 +1,11 @@
 package com.alisonsfa.SaltoUrl.controller;
 
 import java.net.URI;
-import java.util.UUID;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
